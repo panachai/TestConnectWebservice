af5f9079e22a70ae0b3a9de8ff88de9c0dbdca0b
panachai <panachai.ny@gmail.com>
2017-03-18 23:06:31 +0700

    https://cpanel.hostinger.in.th/hosting/index/aid/23485061
    project
    - OKHTTP					ไว้ใช้จริง
    - TestConnectWebservice		ไว้อ่านใช้งาน ฟิลต่างๆ

--- app/src/main/java/com/panachai/testconnectwebservice/MainActivity.java
+++ app/src/main/java/com/panachai/testconnectwebservice/MainActivity.java
@@ -1,13 +1,112 @@
 package com.panachai.testconnectwebservice;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView jsonListview;
+    private ArrayList<String> exData;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jsonListview = (ListView) findViewById(R.id.json_listview);
+
+        exData = new ArrayList<String>();
+
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setCancelable(false);
+                progressDialog.setMessage("Download ...");
+                progressDialog.show();
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    URL url = new URL("http://consolesaleth.esy.es/json/test_json.php"); //  http://thaicfp.com/webservices/json-example.php
+                    URLConnection urlConnection = url.openConnection();
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+                    httpURLConnection.setAllowUserInteraction(false);
+                    httpURLConnection.setInstanceFollowRedirects(true);
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.connect();
+
+                    InputStream inputStream = null;
+                    if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        inputStream = httpURLConnection.getInputStream();
+                    }
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,
+                            "iso-8859-1"), 8);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = null;
+
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line + "\n");
+
+                    }
+                    inputStream.close();
+                    Log.d("JSON Result : ", stringBuilder.toString());
+
+                    //ใส่ ค่าที่ get ออกมาใส่ json array
+                    JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+                    JSONArray exArray = jsonObject.getJSONArray("students");
+
+                    for (int i = 0; i < exArray.length(); i++) {
+                        JSONObject jsonObj = exArray.getJSONObject(i);
+                        exData.add(jsonObj.getString("student_name"));
+                    }
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                progressDialog.dismiss();
+                ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout
+                        .simple_list_item_1, android.R.id.text1, exData);
+                jsonListview.setAdapter(myAdapter);
+            }
+        }.execute();
+
+
     }
+
 }
